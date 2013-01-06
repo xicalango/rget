bae6eb83bc14bd7ac355bba2021fe46c08c323a3
Alexander Weld/logout34 <alex.weld@gmx.net>
2013-01-06 15:57:04 +0100

    prototype working

--- rget.py
+++ rget.py
@@ -3,6 +3,7 @@
 import subprocess
 import re
 from threading import Thread
+from cmd import Cmd
 
 class WGetInfo(object):
 	STATUS_REGEX = re.compile("[ \t]*(?P<data>[0-9]+)(?P<data_unit>[ KMG])[ '.']*(?P<percent>[0-9]+)%[ ]*(?P<speed>[0-9]+)(?P<speed_unit>[ KMG])[ =](?P<time>[0-9dhms ]+)")
@@ -41,7 +42,7 @@ class WGetProcess(Thread):
 	STAT_FIN_BAD = 3
 	STAT_FIN_USER = 4
 
-	def __init__(self, url, outputDir = '.'):
+	def __init__(self, url, outputDir = None):
 		Thread.__init__(self, name='WGet ' + url)
 		self.url = url
 		self.outputDir = outputDir
@@ -50,9 +51,23 @@ class WGetProcess(Thread):
 		self.status = WGetProcess.STAT_IDLE
 		self.exit_status = None
 
+	def getParams(self):
+		params = ['wget', '--progress=dot']
+
+		if self.outputDir != None:
+			params.append('-P')
+			params.append(self.outputDir)
+
+		params.append(self.url)
+
+		return params
+
 	def run(self):
 		self.running = True
-		self.proc = subprocess.Popen(['wget', '--progress=dot', '-P', self.outputDir, self.url], stderr=subprocess.PIPE)
+
+		params = self.getParams()
+
+		self.proc = subprocess.Popen(params, stderr=subprocess.PIPE)
 
 		self.status = WGetProcess.STAT_DL
 
@@ -61,13 +76,70 @@ class WGetProcess(Thread):
 			self.info.update(line)
 
 		if self.running == False:
-			self.status = STAT_FIN_USER
+			self.status = WGetProcess.STAT_FIN_USER
 			self.proc.kill()
 		else:
 			self.running = False
 			if self.proc.returncode == 0:
-				self.status = STAT_FIN_GOOD
+				self.status = WGetProcess.STAT_FIN_GOOD
 			else:
-				self.status = STAT_FIN_BAD
+				self.status = WGetProcess.STAT_FIN_BAD
 
 
+class Manager(object):
+	def __init__(self):
+		self.processes = []
+
+	def add(self, url, outputDir = None):
+		new_process = WGetProcess( url, outputDir = outputDir )
+		self.processes.append( new_process )
+		return new_process
+
+	def addAndStart(self, url, outputDir = None):
+		process = self.add(url, outputDir)
+		process.start()
+	
+	def printAllInfo(self):
+		for p in self.processes:
+			print p.url
+			print p.status
+			print p.info
+	
+	def shutdown(self):
+		for p in self.processes:
+			p.running = False
+			p.join()
+
+
+class RGetConsole(Cmd):
+	def __init__(self, manager):
+		Cmd.__init__(self)
+		self.prompt = "> "
+		self.manager = manager
+
+	def do_add(self, url):
+		self.manager.add( url )
+		return False
+	
+	def do_run(self, url):
+		self.manager.addAndStart( url )
+		return False
+
+	def do_info(self, line):
+		self.manager.printAllInfo()
+		return False
+
+	def do_EOF(self, line):
+		return True
+
+
+def main():
+	manager = Manager()
+
+	console = RGetConsole( manager )
+	console.cmdloop()
+
+	manager.shutdown()
+
+if __name__ == '__main__':
+	main()
